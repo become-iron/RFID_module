6fcf18e91d20c2cd385bb77353c1d4c7994f8069
Alex <sasha-ushakov@mail.ru>
2017-03-02 19:45:35 +0300

    Возвращение результата в виде HTTP-кода

--- module/server.py
+++ module/server.py
@@ -14,116 +14,100 @@ def index():
     return "Hello, reader!"
 
 
+@app.route('/readers/', methods=['POST'])
+def add_reader():
+    """Добавляет настройки для ридера"""
+    # curl -i -H "Content-Type: application/json" -X POST -d "{"""reader_id""": """1""", """bus_addr""": 1, """port_number""": 1}" http://localhost:5000/readers/
+
+    http_code = 201     # создан новый ресурс (с настройками)
+    response = Readers.add_reader(request.json)
+
+    if 'error' in response:
+        http_code = 400     # ошибка в запросе, ошибки в названиях полей, передан не json
+    return jsonify(response), http_code
+
+
 @app.route('/readers/', methods=['GET'])
 def get_readers():
     """Возвращает список настроек для ридеров"""
     # curl -i -X GET http://localhost:5000/readers/
     # curl -i http://localhost:5000/readers/
 
-    # 200 - возвращен список настроек
-    # 404 - по данному адресу ничего не найдено
-    # 405 - метод не соответсвует адресу
-    return jsonify(Readers.get_readers())
-
-
-@app.route('/readers/', methods=['POST'])
-def add_reader():
-    """Добавляет настройки для ридера"""
-    # curl -i -H "Content-Type: application/json" -X POST -d "{"""reader_id""": """1""", """bus_addr""": 1, """port_number""": 1}" http://localhost:5000/readers/
-
-    # 201 - создан новый ресурс (с настройками)
-    # 400 - ошибка в запросе, ошибки в названиях полей, передан не json
-    # 404 - по данному адресу ничего не найдено
-    # 405 - метод не соответсвует адресу
-    return jsonify(Readers.add_reader(request.json))
-
-
-
-@app.route('/readers/', methods=['PUT'])
-def update_readers():
-    """Заменяет все настройки ридеров переданными"""
-
-    # 200 - настройки обновлены
-    # 400 - ошибка в запросе, ошибки в названиях полей, передан не json
-    # 404 - по данному адресу ничего не найдено
-    # 405 - метод не соответсвует адресу
-    return jsonify({})
-
-
-@app.route('/readers/', methods=['DELETE'])
-def delete_readers():
-    """Удаляет все настройки ридеров"""
-
-    # 200 - настройки удалены
-    # 404 - по данному адресу ничего не найдено
-    # 405 - метод не соответсвует адресу
-    return jsonify({})
+    http_code = 200     # возвращен список настроек
+    response = Readers.get_readers()
+    
+    return jsonify(response), http_code
 
 
 @app.route('/readers/<reader_id>/', methods=['GET'])
 def get_reader(reader_id):
     """Возвращает настройки ридера"""
+    # curl -i -X GET http://localhost:5000/readers/1/
+    # curl -i http://localhost:5000/readers/1/
 
-    # 200 - возвращен список настроек
-    # 404 - по данному адресу ничего не найдено
-    # 405 - метод не соответсвует адресу
-    return jsonify({})
+    http_code = 200     # возвращен список настроек
+    response = Readers.get_reader(reader_id)
+
+    return jsonify({}), http_code
+
+
+@app.route('/readers/', methods=['PUT'])
+def update_readers():
+    """Заменяет все настройки ридеров переданными"""
+    # curl -i -H "Content-Type: application/json" -X POST -d "{"""reader_id""": """1""", """bus_addr""": 1, """port_number""": 1}" http://localhost:5000/readers/
+
+    http_code = 200     # настройки обновлены
+    response = Readers.update_readers()
+
+    if 'error' in response:
+        http_code = 400     # ошибка в запросе, ошибки в названиях полей, передан не json
+
+    return jsonify({}), http_code
 
 
 @app.route('/readers/<reader_id>/', methods=['PUT'])
 def update_reader(reader_id):
     """Обновляет настройки ридера"""
 
-    # 200 - настройки обновлены
-    # 400 - ошибка в запросе, ошибки в названиях полей, передан не json
-    # 404 - по данному адресу ничего не найдено
-    # 405 - метод не соответсвует адресу
+    http_code = 200     # настройки обновлены
+    response = Readers.update_reader(reader_id)
+
+    if 'error' in response:
+        http_code = 400     # ошибка в запросе, ошибки в названиях полей, передан не json
+
+    return jsonify({}), http_code
+
+
+@app.route('/readers/', methods=['DELETE'])
+def delete_readers():
+    """Удаляет все настройки ридеров"""
+    # curl -i -X DELETE http://localhost:5000/readers/
+
+    http_code = 200     # настройки удалены
+    response = Readers.delete_readers()
+
     return jsonify({})
 
 
 @app.route('/readers/<reader_id>/', methods=['DELETE'])
 def delete_reader(reader_id):
     """Удаляет ридер"""
+    # curl -i -X DELETE http://localhost:5000/readers/1/
 
-    # 200 - настройки удалены
-    # 404 - по данному адресу ничего не найдено
-    # 405 - метод не соответсвует адресу
-    return jsonify({})
+    http_code = 200     # настройки удалены
+    response = Readers.delete_reader(reader_id)
 
-
-@app.route('/readers/<reader_id>/start/', methods=['PUT'])
-# @app.route('/readers/<reader_id>/', methods=['PUT'])
-def start_reader(reader_id):
-    """Подключает ридер"""
-
-    # 200 - ридер подключен
-    # 400 - ошибка в запросе, ошибки в названиях полей, передан не json
-    # 404 - по данному адресу ничего не найдено
-    # 405 - метод не соответсвует адресу
-    return jsonify({})
-
-
-@app.route('/readers/<reader_id>/stop/', methods=['PUT'])
-# @app.route('/readers/<reader_id>/', methods=['PUT'])
-def stop_reader(reader_id):
-    """Отключает ридер"""
-
-    # 200 - ридер отключен
-    # 400 - ошибка в запросе, ошибки в названиях полей, передан не json
-    # 404 - по данному адресу ничего не найдено
-    # 405 - метод не соответсвует адресу
     return jsonify({})
 
 
 # Эта функция, скорее всего, не нужна
 @app.route('/readers/<reader_id>/tags/inventory/', methods=['GET'])
-# @app.route('/readers/<reader_id>/inventory/', methods=['GET'])
 def inventory(reader_id):
     """Возвращает идентификаторы меток"""
 
-    # 200 - идентификаторы возвращены
-    # 404 - по данному адресу ничего не найдено
-    # 405 - метод не соответсвует адресу
+    http_code = 200     # идентификаторы возвращены
+    response = Readers.inventory(reader_id)
+
     return jsonify({})
 
 
@@ -131,9 +115,9 @@ def inventory(reader_id):
 def read_tags(reader_id):
     """Возвращает информацию с меток"""
 
-    # 200 - информация возвращена
-    # 404 - по данному адресу ничего не найдено
-    # 405 - метод не соответсвует адресу
+    http_code = 200     # информация возвращена
+    response = Readers.read_tags(reader_id)
+
     return jsonify({})
 
 
@@ -141,20 +125,23 @@ def read_tags(reader_id):
 def write_tags(reader_id):
     """Записывает информацию в метки"""
 
-    # 200 - информация записана
-    # 400 - ошибка в запросе, ошибки в названиях полей, передан не json
-    # 404 - по данному адресу ничего не найдено
-    # 405 - метод не соответсвует адресу
+    http_code = 200     # информация записана
+    response = Readers.write_tags(reader_id)
+
+    if 'error' in response:
+        http_code = 400     # ошибка в запросе, ошибки в названиях полей, передан не json
+
     return jsonify({})
 
 
 @app.route('/readers/<reader_id>/tags/', methods=['DELETE'])
 def clear_tags(reader_id):
     """Очищает информацию с меток"""
+    # curl -i -X DELETE http://localhost:5000/readers/tags/
 
-    # 200 - информация очищена
-    # 404 - по данному адресу ничего не найдено
-    # 405 - метод не соответсвует адресу
+    http_code = 200     # 
+    response = Readers.clear_tags(reader_id)
+
     return jsonify({})
 
 
